48b564ba990e5cce30a3a9df636538a49e152dcd
Dinar Ibragimov <dinaribragimov@Dianr-Ibragimovs-MacBook-Air.local>
2026-04-29 11:26:03 -0700

    SCRUM-51-add user routes

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,3 +1,41 @@
 /*
-    Here will be. So magic !!!Дописать will communcate with Controllers and Models
+    Here will be. So magic doors!!!Дописать will communcate with Controllers and Models
 */
+
+import {Router} from 'express';
+import { authenticateUser } from '../middleware/authentication';
+import {
+    showCurrentUser, 
+    updateUser, 
+    updateUserPassword,
+    deleteUser,
+    getUserVisuals,
+    addVisualToCollection,
+    removeVisualFromCollection,
+} from '../controllers/user';
+
+const router = Router();
+
+// All routes are req auth 
+router.use(authenticateUser)
+
+// User profile , get, edit, remove
+router.route('/user')
+    .get(showCurrentUser)
+    .patch(updateUser)
+    .delete(deleteUser);
+
+// Change password
+router.route('/user/password')
+    .patch(updateUserPassword);
+
+// List of visuals
+router.route('/user/visuals')
+    .get(getUserVisuals);
+
+// Manage with particular visual, remove , add
+router.route('/user/visuals/:id')
+    .post(addVisualToCollection)
+    .delete(removeVisualFromCollection);
+
+export default router;
