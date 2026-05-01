cca46f869fef07ca27970820085518033d3646d9
Dinar Ibragimov <dinaribragimov@Dianr-Ibragimovs-MacBook-Air.local>
2026-05-01 09:48:13 -0700

    SCRUM-49-fix auth controller to use real DB operations

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { UnauthenticatedError, BadRequestError } from '../errors';
-import { createJWT, attachCookiesToResponse } from '../utils/jwt';
+import { attachCookiesToResponse } from '../utils/jwt';
+import User from '../models/User';
 
-//  model User, when it done
-// import User from '../models/User';
-
+// Register a new user 
 export const register = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
 
@@ -13,16 +12,19 @@ export const register = async (req: Request, res: Response) => {
     throw new BadRequestError('Please provide name, email and password');
   }
 
-  // Temp obj 
-  const user = { _id: 'temp_id', name, email }; 
-  const tokenUser = { userId: user._id, name: user.name, email: user.email };
-  
-  const token = createJWT(tokenUser);
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new BadRequestError('Email already exists');
+  }
+
+  const user = await User.create({ name, email, password });
+  const token = user.createJWT();
   attachCookiesToResponse(res, token);
 
   res.status(StatusCodes.CREATED).json({ data: user });
 };
 
+// Login user 
 export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
@@ -30,20 +32,28 @@ export const login = async (req: Request, res: Response) => {
     throw new BadRequestError('Please provide email and password');
   }
 
-  // Temp obj for test
-  const user = { _id: 'temp_id', name: 'test user', email };
-  const tokenUser = { userId: user._id, name: user.name, email: user.email };
-  
-  const token = createJWT(tokenUser);
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+
+  const token = user.createJWT();
   attachCookiesToResponse(res, token);
 
   res.status(StatusCodes.OK).json({ data: user });
 };
 
+// Logout user 
 export const logout = async (req: Request, res: Response) => {
   res.cookie('token', 'logout', {
     httpOnly: true,
     expires: new Date(Date.now()),
+    signed: true,
   });
   res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
 };
