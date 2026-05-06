873e0a3e1708f7870bfc2a19280af920370dff0b
Joe Siburov <g.siburov@gmail.com>
2026-05-06 14:18:09 -0700

    UI: login/signup pages are prepared

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,12 +1,14 @@
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 export default function LoginPage() {
     return (
-        <div>
+        <div className="flex flex-col h-screen justify-between">
             <NavBar />
-            <div className="flex items-center justify-center h-screen">
-                <h1 className="text-xl">Login Page</h1>
+            <div className="flex items-center justify-center bg-void h-[calc(100%-64px-48px)]">
+                <h1 className="text-xl text-text-primary">Login Page</h1>
             </div>
+            <Footer />
         </div>
     );
 }
--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,12 +1,14 @@
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 export default function SignUpPage() {
     return (
-        <div>
+        <div className="flex flex-col h-screen justify-between">
             <NavBar />
-            <div className="flex items-center justify-center h-screen">
-                <h1 className="text-xl">Sign Up Page</h1>
+            <div className="flex items-center justify-center bg-void h-[calc(100%-64px-48px)]">
+                <h1 className="text-xl text-text-primary">Sign Up Page</h1>
             </div>
+            <Footer />
         </div>
     );
 }
