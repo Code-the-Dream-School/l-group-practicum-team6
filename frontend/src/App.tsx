70f194ac932012327937760ab931d59c3f84a0b5
Lili-Kiwi <bill2017bill2017@gmail.com>
2026-05-08 14:39:17 -0400

    feat: navbar component (SCRUM-72) (#26)
    * feat: build navbar component with mobile menu (SCRUM-72)
    
    * feat: add navbar and footer to other pages (SCRUM-72)
    
    * refactor: replace default avatar svg with initial (SCRUM-72)
    
    * feat: gradient avatar (SCRUM-72)
    
    * refactor: rename GuestActions to AuthLinks (SCRUM-72)
    
    * refactor: move pickGradient to utils (SCRUM-72)
    
    * refactor: simplify MobileMenu link classes (SCRUM-72)
    
    * refactor: centralize routes in Routes enum (SCRUM-72)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,15 +11,16 @@ import NotFoundPage from "./pages/NotFoundPage";
 
 import ProtectedRoute from "./routes/ProtectedRoute";
 import GuestRoute from "./routes/GuestRoute";
+import { Routes as RoutePaths } from "./routes/paths";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path={RoutePaths.HOME} element={<LandingPage />} />
 
         <Route
-        path="/login"
+        path={RoutePaths.LOGIN}
         element={
           <GuestRoute>
             <LoginPage />
@@ -28,7 +29,7 @@ function App() {
         />
 
         <Route
-        path="/signup"
+        path={RoutePaths.SIGNUP}
         element={
           <GuestRoute>
             <SignUpPage />
@@ -38,7 +39,7 @@ function App() {
         />
 
         <Route
-          path="/explore"
+          path={RoutePaths.EXPLORE}
           element={
             <ProtectedRoute>
               <ExplorePage />
@@ -46,10 +47,10 @@ function App() {
           }
         />
 
-        <Route path="/visualizer/demo" element={<DemoPlayerPage />} />
+        <Route path={RoutePaths.VISUALIZER_DEMO} element={<DemoPlayerPage />} />
 
         <Route
-        path="/visualizer/:id"
+        path={RoutePaths.VISUALIZER}
         element={
           <ProtectedRoute>
             <PlayerPage />
@@ -58,7 +59,7 @@ function App() {
         />
 
         <Route
-        path="/my-visuals"
+        path={RoutePaths.MY_VISUALS}
         element={
           <ProtectedRoute>
             <MyVisualsPage />
@@ -66,7 +67,7 @@ function App() {
         }
         />
 
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path={RoutePaths.NOT_FOUND} element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/MyVisualsPage.tsx
+++ frontend/src/pages/MyVisualsPage.tsx
@@ -1,3 +1,14 @@
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+
 export default function MyVisualsPage() {
-    return <div>My Visuals Page</div>;
+    return (
+        <div className="flex h-screen flex-col justify-between">
+            <NavBar />
+            <div className="flex items-center justify-center bg-void flex-1">
+                <h1 className="text-xl text-text-primary">My Visuals Page</h1>
+            </div>
+            <Footer />
+        </div>
+    );
 }
