dba1383269819bbfd1a3fd82b49f455790e1a655
Lili-Kiwi <bill2017bill2017@gmail.com>
2026-05-26 01:23:51 -0400

    fix: fixed test

--- frontend/src/context/useToast.ts
+++ frontend/src/context/useToast.ts
@@ -6,7 +6,7 @@ export function useToast() {
   const context = useContext(ToastContext);
 
   if (!context) {
-    throw new Error('useToast must be used within ToastProvider');
+    throw new Error('useToast must be used within a ToastProvider');
   }
 
   return context;
